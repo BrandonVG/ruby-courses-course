8804aa65ee3383a85c88d858a0756f37269eaac2
Brandon Vadillo <bvadillo0@ucol.mx>
2023-06-13 21:39:57 -0500

    changes2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
+  resources :users
   root 'static_pages#landing_page'
   get 'privacy-policy', to: 'static_pages#privacy_policy'
 end
