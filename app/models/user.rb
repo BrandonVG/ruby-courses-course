d2abbea94799fd5ba4339a4b09db860794f4f314
Brandon Vadillo <bvadillo0@ucol.mx>
2023-06-14 14:11:57 -0500

    Ransack for courses

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,12 @@ class User < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "current_sign_in_at", "current_sign_in_ip", "email", "id", "last_sign_in_at", "last_sign_in_ip", "remember_created_at", "reset_password_sent_at", "sign_in_count", "updated_at"]
   end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["courses"]
+  end
+
+  def username
+    self.email.split(/@/).first
+  end
 end
