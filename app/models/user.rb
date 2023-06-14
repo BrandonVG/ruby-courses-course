97e68ae064ca562eb4ff68e601fbc8625caca86d
Brandon Vadillo <bvadillo0@ucol.mx>
2023-06-14 16:15:46 -0500

    Added rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,17 @@ class User < ApplicationRecord
   def username
     self.email.split(/@/).first
   end
+
+  after_create :assign_default_role
+  def assign_default_role
+    if User.count == 1
+      self.add_role(:admin) if self.roles.blank?
+      self.add_role(:teacher)
+      self.add_role(:student)
+    else
+      self.add_role(:student) if self.roles.blank?
+      self.add_role(:teacher)
+
+    end
+  end
 end
