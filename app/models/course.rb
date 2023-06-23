2a271caa8cd2cb06b9b0557e330094c23c84667c
Brandon Vadillo <bvadillo0@ucol.mx>
2023-06-23 17:19:57 -0500

    Course rating

--- app/models/course.rb
+++ app/models/course.rb
@@ -35,6 +35,14 @@ class Course < ApplicationRecord
     self.enrollments.where(user_id: [user.id], course_id: [self.id]).empty?
   end
   
+  def update_rating
+    if enrollments.any? && enrollments.where.not(rating: nil).any?
+      update_column :average_rating, (enrollments.average(:rating).round(2).to_f)
+    else
+      update_column :average_rating, (0)
+    end
+  end
+  
   include PublicActivity::Model
   tracked owner: Proc.new{ |controller, model| controller.current_user }
 
