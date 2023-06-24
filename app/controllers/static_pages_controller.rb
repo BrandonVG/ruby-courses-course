f091adcf451d265d3a024c3ba846d67f7918bdf5
Brandon Vadillo <bvadillo0@ucol.mx>
2023-06-23 23:56:04 -0500

    Dynamic & Personalized Homepage (Popular, New, Top rated Courses)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,11 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:landing_page]
   def landing_page
     @courses = Course.all.limit(3)
-    @recent_courses = Course.all.limit(3).order(created_at: :desc)
+    @latest_couses = Course.all.limit(3).order(created_at: :desc)
+    @latest_good_reviews = Enrollment.reviewed.order(rating: :desc, created_at: :desc).limit(3)
+    @top_rated_courses = Course.order(average_rating: :desc, created_at: :desc).limit(3)
+    @popular_courses = Course.order(enrollments_count: :desc, created_at: :desc).limit(3)
+    @purchased_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).order(created_at: :desc).limit(3)
   end
 
   def privacy_policy
