134b388fedcc1a52c5879fe6b1102189aaa26673
Brandon Vadillo <bvadillo0@ucol.mx>
2023-06-24 12:44:35 -0500

    Validation for Destroy with dependent records (restrict_with_error, n…

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,7 +6,7 @@ class Course < ApplicationRecord
   #User.find_each { |user| User.reset_counters(user.id, :courses) }  
   has_rich_text :description
   has_many :lessons, dependent: :destroy
-  has_many :enrollments
+  has_many :enrollments, dependent: :restrict_with_error
   has_many :user_lessons, through: :lessons
 
   scope :latest, -> { limit(3).order(created_at: :desc) }
