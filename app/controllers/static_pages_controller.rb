8f50dfd8311358d27830056ce6d56a4e1dcec13c
Brandon Vadillo <bvadillo0@ucol.mx>
2023-06-24 14:06:28 -0500

    load charts without timeouts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -20,12 +20,6 @@ class StaticPagesController < ApplicationController
   end
 
   def analytics
-    if current_user.has_role?(:admin)
-      @users = User.all
-      @enrollments = Enrollment.all
-      @courses = Course.all
-    else
-      redirect_to root_path, alert: "You are not authorized to access this page"
-    end
+    redirect_to root_path, alert: "You are not authorized to access this page" unless current_user.has_role?(:admin)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   get 'privacy-policy', to: 'static_pages#privacy_policy'
   get 'activity', to: 'static_pages#activity'
   get 'analytics', to: 'static_pages#analytics'
+  get 'charts/users_per_day', to: 'charts#users_per_day'
+  get 'charts/enrollments_per_day', to: 'charts#enrollments_per_day'
+  get 'charts/course_popularity', to: 'charts#course_popularity'
 end
