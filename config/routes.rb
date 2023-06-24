93c1f73b99c5fe97f8e1efdea93327348644ed32
Brandon Vadillo <bvadillo0@ucol.mx>
2023-06-24 14:14:00 -0500

    namespace charts in routes, group by sum

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,29 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
+  root 'static_pages#landing_page'
+  get 'privacy-policy', to: 'static_pages#privacy_policy'
+  get 'activity', to: 'static_pages#activity'
+  get 'analytics', to: 'static_pages#analytics'
+
   resources :enrollments do
     get :my_students, on: :collection
   end
-  devise_for :users
+
   resources :courses do
     get :purchased, :pending_review, :created, on: :collection
     resources :lessons
     resources :enrollments, only: [:new, :create]
   end
+
   resources :users, only: [:index, :edit, :show, :update]
-  root 'static_pages#landing_page'
-  get 'privacy-policy', to: 'static_pages#privacy_policy'
-  get 'activity', to: 'static_pages#activity'
-  get 'analytics', to: 'static_pages#analytics'
-  get 'charts/users_per_day', to: 'charts#users_per_day'
-  get 'charts/enrollments_per_day', to: 'charts#enrollments_per_day'
-  get 'charts/course_popularity', to: 'charts#course_popularity'
+  
+  namespace :charts do
+    get 'users_per_day'
+    get 'enrollments_per_day'
+    get 'course_popularity'
+    get 'money_makers'
+  end
+
 end
