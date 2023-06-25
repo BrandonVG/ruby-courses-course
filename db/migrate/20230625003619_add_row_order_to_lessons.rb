aaa4608e78555033469c6936756c24b64d5fae34
Brandon Vadillo <bvadillo0@ucol.mx>
2023-06-24 21:06:44 -0500

    gem ranked_model - give lessons inside a course a number

--- db/migrate/20230625003619_add_row_order_to_lessons.rb
+++ db/migrate/20230625003619_add_row_order_to_lessons.rb
@@ -1,5 +1,6 @@
 class AddRowOrderToLessons < ActiveRecord::Migration[7.0]
   def change
     add_column :lessons, :row_order, :integer
+    Lesson.update_all('row_order = EXTRACT(EPOCH FROM created_at)')
   end
 end
